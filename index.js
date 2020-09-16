ad4424c7f1cc9ad7f5e191773944c82d196a476f
akhilamartia <akhila.martia@gmail.com>
2020-09-17 01:17:46 +0530

    NodeJS project

---
+++ index.js
@@ -0,0 +1,97 @@
+// Blocking, synchronous way
+const fs = require("fs");
+const http = require("http");
+
+////////
+// FILES
+
+/////////
+// SERVER
+
+const _replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
+  return output;
+};
+
+const templateOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const templateProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+const templateCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  // Overview Page
+  if (pathName === "/" || pathName === "/overview") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+
+    const cardsHtml = dataObj.map((obj) => _replaceTemplate(templateCard, obj)).join("");
+    const output = templateOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
+
+    // Product Page
+  } else if (pathName === "/product") {
+    res.end("This is a PRODUCT");
+    // API
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+    });
+    res.end(data);
+    // Not Found
+  } else {
+    res.writeHead(404, {
+      "Content-Type": "text/html",
+      "my-own-header": "Nothing is working",
+    });
+    res.end("Page not found!");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to incoming requests on port 8000");
+});
+
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+
+// const textOut = `This is what we know about Avocados: ${textIn}.\n Created on: ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File Written");
+
+// // Non-blocking, asynchronous way
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("ERROR!");
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("File has been written!");
+//       });
+//     });
+//   });
+// });
+// console.log("Reading file asynchronously...");
